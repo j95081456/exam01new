d9204d251a88b54f474e99d4e419f3e9218a5dd4
j95081456 <j95081456@gmail.com>
2020-04-01 17:32:14 +0800

    12

--- pro/main.cpp
+++ pro/main.cpp
@@ -4,6 +4,14 @@
 uLCD_4DGL uLCD(D1, D0, D2);
 PwmOut PWM1(D6);
 
+Serial pc( USBTX, USBRX );
+AnalogOut Aout(D7);
+
+int sample = 128;
+int i;
+float ADCdata[128];
+
+
 int main()
 {
       uLCD.printf("\n107061246\n"); //Default Green on black text
@@ -11,16 +19,29 @@ int main()
       uLCD.line(20,20,40,20,RED);
       uLCD.line(20,40,40,40,RED);
       uLCD.line(40,20,40,40,RED);
-      while (1){
+      //while (1){
     for (int i =0; i < 10 ; i++){
       PWM1.period(0.01);
       PWM1 = 0.1*i;
+      Aout = PWM1;
       wait(100);
     }
     for (int i =10; i > 0 ; i--){
       PWM1.period(0.01);
       PWM1 = 0.1*i;
+      Aout = PWM1;
       wait(100);
-    }
+    //}
+
       }
+      
+  for (i = 0; i < sample; i++){
+    Aout = Ain;
+    ADCdata[i] = Ain;
+    wait(1./sample);
+  }
+  for (i = 0; i < sample; i++){
+    pc.printf("%1.3f\r\n", ADCdata[i]);
+    wait(0.1);
+  }
 }
